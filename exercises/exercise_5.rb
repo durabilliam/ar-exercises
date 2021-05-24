08ebdb5bbe21a26350ffbfb1c47de8d618d4f191
William Perry <durabilliam@gamil.com>
2021-05-24 20:09:35 -0230

    add exercise5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,6 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts "Total Annual Revenue for all stores: $#{Store.sum(:annual_revenue)}"
+puts "Average Annual Revenue for all stores: $#{Store.average(:annual_revenue)}"
+puts "Number of stores with Annual Revenue greater than 1 million: #{Store.where('annual_revenue < ?', 1000000).count}"
